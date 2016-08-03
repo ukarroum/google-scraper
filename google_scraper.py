d2e71b7ce2dd8dffd9a54cc9de0918067d9c852c
Yassir Karroum <ukarroum17@gmail.com>
2016-08-03 15:52:48 +0100

    Ajout du moteur StartPage pour contourner les protections mise coté Google.

--- google_scraper.py
+++ google_scraper.py
@@ -26,10 +26,10 @@ nb_req_per_page = 10
 tor_process = 0
 use_tor = False   # Si tor est utilisé le parametre devient true
 
-#En choisissant de passer par les relais d'un meme pays
-#Les résultats deviennent plus cogérents entre eux.
+# En choisissant de passer par les relais d'un meme pays
+# Les résultats deviennent plus cogérents entre eux.
 
-use_contry_code = False
+use_contry_code = True
 country_code = "fr"  # Un choix arbitraire qui est du à un nombre plutot elevé des relais francais.
 
 
@@ -48,10 +48,60 @@ def print_help():
 
     print("Options : ")
     print("\t QUERY : La requette en question ex : program, site:.ma, inurl:index.php, etc.")
-    print("\t--searchEngine=SEARCH ENGINE\t Le moteur de recherche en à utiliser : Google, Bing")
+    print("\t--searchEngine=SEARCH ENGINE\t Le moteur de recherche en à utiliser : Google, Bing, StartPage (Proxy Google)")
     print("\t--begin=X\t Par où commencer la recherche ")
     print("\t--number=Y\t Nombre de résultats souhaités")
     print("\t--fileout=FILE\t Le fichier ou le texte en sortie sera stoké (par défaut sortie standard")
+
+
+def start_page_scrap(query, begin=0, n=10):
+    """
+        StartPage Scrap : Permet de récupérer le résultat depuis le moteur google en passant par startpage.
+
+    :param query: La requette de recherche, par example : python, site:.ma, inurl:index.php
+    :param begin: Par où commencer la recherche .
+    :param n: Nombre de résultats souhaités
+    :return: Liste contenant les liens.
+    """
+
+    links = []
+    iterations = {"success": 0, "failure": 0}
+
+    start = begin
+
+    while len(links) < n:
+
+        # Les parametres dont je connais l'utilité sont :
+        # prf : Contient les préférences de recherche (nombre de résutats par page) sans passer par les cookies.
+        # language : permet de fixer le language en anglais quelquesoit l'ip source.
+        #            pratique pour utiliser des phrases anglaises dans les conditions au lieu de chercher des balises.
+        # Les autres devront etre enlevés par la suite.
+        soup = BeautifulSoup(get_html("https://www.startpage.com/do/search?cmd=process_search&language=english&qid=NBLORKMNPMTQ989HVWJUE&rcount=&rl=NONE&abp=-1&t=air&query="+query+"&cat=web&engine0=v1all&startat"+str(start)), "html.parser")
+
+        if "Sorry, there are no Web results for this search!" in soup:
+            print(term.format("Vous atteint la limite des résultats de recherche .\n", term.Color.RED))
+            break
+
+        if len(soup.find_all('span', class_="url")):
+            iterations["success"] += 1
+        else:
+            iterations["failure"] += 1
+            new_identity()
+            continue
+
+        for span in soup.find_all('span', class_="url"):
+            links.append(span.string)
+            sys.stdout.write("\r" + OKGREEN + "Avancement : " + str(len(links)) + " / " + str(n) + ENDC)
+            sys.stdout.flush()
+
+        print()
+        start += nb_req_per_page
+
+    if use_tor:
+        print(term.format("Arret de Tor.\n", term.Attr.BOLD))
+        kill_tor()
+
+    return links[:n]
 
 
 def google_scrap(query, begin=0, n=10):
@@ -191,7 +241,7 @@ def get_html(url):
         query.setopt(pycurl.PROXYPORT, SOCKS_PORT)
         query.setopt(pycurl.PROXYTYPE, pycurl.PROXYTYPE_SOCKS5_HOSTNAME)
 
-    query.setopt(pycurl.USERAGENT, 'Mozilla/5.0')
+    query.setopt(pycurl.USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0')
     query.setopt(pycurl.WRITEFUNCTION, output.write)
 
     try:
@@ -248,6 +298,8 @@ if __name__ == "__main__":
         links = google_scrap(query, int(g_opts["begin"]), int(g_opts["number"]))
     elif g_opts['searchEngine'] == 'Bing':
         links = bing_scrap(query, int(g_opts["begin"]), int(g_opts["number"]))
+    elif g_opts['searchEngine'] == 'StartPage':
+        links = start_page_scrap(query, int(g_opts["begin"]), int(g_opts["number"]))
 
     save_file(links, g_opts['fileout'])
 
